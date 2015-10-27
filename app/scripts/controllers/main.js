da0a2dd4b1e79c62c64200578881259054954ada
toanhtran <toanh_tran@yahoo.com>
2015-10-27 17:58:22 +0900

    addTodo and removeTodo function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
   });
